bc57ad83ef1581b063c427a40722fbebd660ad35
DaliborTrampota <dalibor.trampota@gmail.com>
2020-11-11 20:12:50 +0100

    Added getTracks function
    getTracks formats data from getdata function into uniform array of tracks

--- index.js
+++ index.js
@@ -5,113 +5,125 @@ const { parse } = require("himalaya");
 const SUPPORTED_TYPES = ["album", "artist", "episode", "playlist", "track"];
 
 function getData(url) {
-  let parsedURL = {};
-
-  try {
-    parsedURL = spotifyURI.parse(url);
-  } catch (error) {
-    return Promise.reject(new Error(`Couldn't parse ${url} as valid URL`));
-  }
-
-  if (!parsedURL.type) {
-    return Promise.reject(new Error(`Failed to parse ${url} as Spotify URL`));
-  }
-
-  const embedURL = spotifyURI.formatEmbedURL(parsedURL);
-  return fetch(embedURL)
+    let parsedURL = {};
+    
+    try {
+        parsedURL = spotifyURI.parse(url);
+    } catch (error) {
+        return Promise.reject(new Error(`Couldn't parse ${url} as valid URL`));
+    }
+    
+    if (!parsedURL.type) {
+        return Promise.reject(new Error(`Failed to parse ${url} as Spotify URL`));
+    }
+    
+    const embedURL = spotifyURI.formatEmbedURL(parsedURL);
+    return fetch(embedURL)
     .then(res => res.text())
     .then(parse)
     .then(embed => {
-      const scripts = embed
+        const scripts = embed
         .filter(e => e.tagName === "html")[0]
         .children.filter(e => e.tagName === "body")[0]
         .children.filter(e => e.tagName === "script");
-      const resourceScript = scripts.filter(
-        e => e.attributes.findIndex(a => a.value === "resource") !== -1
-      );
-      const hydrateScript = scripts.filter(
-        e => e.children[0] && e.children[0].content.includes(`"data":`)
-      );
-      if (resourceScript.length > 0) {
-        // found data in the older embed style
-        return JSON.parse(
-          decodeURIComponent(resourceScript[0].children[0].content)
-        );
-      } else if (hydrateScript.length > 0) {
-        // found hydration data
-        // parsing via looking for { to be a little bit resistant to code changes
-        const scriptContent = hydrateScript[0].children[0].content;
-        const dataString =
-          "{" + scriptContent.split("{").slice(1).join("{").trim();
-        return JSON.parse(dataString).data;
-      } else {
-        return Promise.reject(
-          new Error(
-            "Couldn't find any data in embed page that we know how to parse"
-          )
-        );
-      }
-    })
-    .then(sanityCheck);
+        const resourceScript = scripts.filter(
+            e => e.attributes.findIndex(a => a.value === "resource") !== -1
+            );
+            const hydrateScript = scripts.filter(
+                e => e.children[0] && e.children[0].content.includes(`"data":`)
+                );
+                if (resourceScript.length > 0) {
+                    // found data in the older embed style
+                    return JSON.parse(
+                        decodeURIComponent(resourceScript[0].children[0].content)
+                        );
+                    } else if (hydrateScript.length > 0) {
+                        // found hydration data
+                        // parsing via looking for { to be a little bit resistant to code changes
+                        const scriptContent = hydrateScript[0].children[0].content;
+                        const dataString =
+                        "{" + scriptContent.split("{").slice(1).join("{").trim();
+                        return JSON.parse(dataString).data;
+                    } else {
+                        return Promise.reject(
+                            new Error(
+                                "Couldn't find any data in embed page that we know how to parse"
+                                )
+                                );
+                            }
+                        })
+                        .then(sanityCheck);
+                    }
+                    
+function parseIntoTrackArray(data){
+    if(data.tracks.items)
+        if(data.tracks.items[0].track)
+            return Promise.resolve(data.tracks.items.map(t => t.track));//playlist
+        else
+            return Promise.resolve(data.tracks.items)//album
+    else(data.tracks)
+        return Promise.resolve(data.tracks)//artist
 }
-
+                    
 function parseIntoPreview(data) {
-  const track = getFirstTrack(data);
-  const images = data.type === "track" ? data.album.images : data.images;
-  const date = data.album ? data.album.release_date : data.release_date;
-
-  return Promise.resolve({
-    date,
-    title: data.name,
-    type: data.type,
-    track: track.name,
-    description: data.description || undefined,
-    artist: track.artists.map(a => a.name).join(" & "),
-    image: images.reduce((a, b) => (a.width > b.width ? a : b)).url,
-    audio: track.preview_url,
-    link: data.external_urls.spotify,
-    embed: `https://embed.spotify.com/?uri=${data.uri}`
-  });
+    const track = getFirstTrack(data);
+    const images = data.type === "track" ? data.album.images : data.images;
+    const date = data.album ? data.album.release_date : data.release_date;
+    
+    return Promise.resolve({
+        date,
+        title: data.name,
+        type: data.type,
+        track: track.name,
+        description: data.description || undefined,
+        artist: track.artists.map(a => a.name).join(" & "),
+        image: images.reduce((a, b) => (a.width > b.width ? a : b)).url,
+        audio: track.preview_url,
+        link: data.external_urls.spotify,
+        embed: `https://embed.spotify.com/?uri=${data.uri}`
+    });
 }
-
+                    
 function getFirstTrack(data) {
-  switch (data.type) {
-    case "track":
-      return data;
-    case "playlist":
-      return data.tracks.items[0].track;
-    case "album":
-      return data.tracks.items[0];
-    case "artist":
-      return data.tracks[0];
-    case "episode":
-      return {
-        artists: data.show.publisher.split(" and ").map(name => ({ name })),
-        name: data.show.name,
-        preview_url: data.audio_preview_url
-      };
-    default:
-      return data;
-  }
+    switch (data.type) {
+        case "track":
+            return data;
+        case "playlist":
+            return data.tracks.items[0].track;
+        case "album":
+            return data.tracks.items[0];
+        case "artist":
+            return data.tracks[0];
+        case "episode":
+            return {
+                artists: data.show.publisher.split(" and ").map(name => ({ name })),
+                name: data.show.name,
+                preview_url: data.audio_preview_url
+            };
+        default:
+            return data;
+    }
 }
-
+                    
 function sanityCheck(data) {
-  if (!data || !data.type || !data.name) {
-    return Promise.reject(
-      new Error("Data doesn't seem to be of the right shape to parse")
-    );
-  }
-
-  if (!SUPPORTED_TYPES.includes(data.type)) {
-    return Promise.reject(
-      new Error(
-        `Not an ${SUPPORTED_TYPES.join(", ")}. Only these types can be parsed`
-      )
-    );
-  }
-  return Promise.resolve(data);
+    if (!data || !data.type || !data.name) {
+        return Promise.reject(
+            new Error("Data doesn't seem to be of the right shape to parse")
+        );
+    }
+    if (!SUPPORTED_TYPES.includes(data.type)) {
+        return Promise.reject(
+            new Error(
+                `Not an ${SUPPORTED_TYPES.join(", ")}. Only these types can be parsed`
+            )
+        );
+    }
+    return Promise.resolve(data);
 }
 
 module.exports.getData = getData;
 
 module.exports.getPreview = url => getData(url).then(parseIntoPreview);
+
+module.exports.getTracks = url => getData(url).then(parseIntoTrackArray);
+                                
