2359879149b779a74b2089c54d351d9004c8de75
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-03-19 19:33:28 +0100

    test: enable all

--- test/get-preview.js
+++ test/get-preview.js
@@ -90,140 +90,140 @@ test('get preview for spotify artist', async t => {
   t.true(preview.link?.includes('open.spotify.com/artist/'))
 })
 
-// test('get preview for spotify album', () => {
-//   expect.assertions(10)
-//   return getPreview(
-//     'https://open.spotify.com/album/4tDBsfbHRJ9OdcMO9bmnai'
-//   ).then(result => {
-//     expect(result.date).toBe('2015-11-27')
-//     expect(result.description).toBeUndefined()
-//     expect(result.title).toBe('PRODUCT')
-//     expect(result.type).toBe('album')
-//     expect(result.artist).toContain('SOPHIE')
-//     expect(result.track).toBeDefined()
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toBeNull()
-//     expect(result.link).toContain('open.spotify.com/album/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
-//     )
-//   })
-// })
+test('get preview for spotify album', async t => {
+  const preview = await getPreview(
+    'https://open.spotify.com/album/4tDBsfbHRJ9OdcMO9bmnai'
+  )
 
-// test('get preview for spotify playlist', () => {
-//   expect.assertions(10)
-//   return getPreview(
-//     'https://open.spotify.com/user/sophiemsmsmsm/playlist/3Q4cPwMHY95ZHXtmcU2xvH'
-//   ).then(result => {
-//     expect(result.date).toBeUndefined()
-//     expect(result.description).toBeUndefined()
-//     expect(result.title).toBe('SOPHIE – PRODUCT')
-//     expect(result.type).toBe('playlist')
-//     expect(result.artist).toContain('SOPHIE')
-//     expect(result.track).toBeDefined()
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toContain('/mp3-preview/')
-//     expect(result.link).toContain('/playlist/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:playlist:3Q4cPwMHY95ZHXtmcU2xvH'
-//     )
-//   })
-// })
+  t.is(preview.date, '2015-11-27')
+  t.is(preview.description, undefined)
+  t.is(preview.title, 'PRODUCT')
+  t.is(preview.type, 'album')
+  t.is(preview.artist.includes('SOPHIE'), true)
+  t.is(preview.track, 'BIPP')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio, null)
+  t.is(preview.link.includes('open.spotify.com/album/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
+  )
+})
 
-// test('get preview for spotify episode', () => {
-//   expect.assertions(10)
-//   return getPreview(
-//     'http://open.spotify.com/episode/64TORH3xleuD1wcnFsrH1E'
-//   ).then(result => {
-//     expect(result.title).toBe('Hasty Treat - Modules in Node')
-//     expect(result.description).toContain('In this Hasty Treat')
-//     expect(result.type).toBe('episode')
-//     expect(result.artist).toContain('Wes Bos & Scott Tolinski')
-//     expect(result.track).toBeDefined()
-//     expect(result.date).toBe('2020-01-06')
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toContain('/mp3-preview/')
-//     expect(result.link).toContain('/episode/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:episode:64TORH3xleuD1wcnFsrH1E'
-//     )
-//   })
-// })
+test('get preview for spotify playlist', async t => {
+  const preview = await getPreview(
+    'https://open.spotify.com/user/sophiemsmsmsm/playlist/3Q4cPwMHY95ZHXtmcU2xvH'
+  )
 
-// test('get preview for spotify album with constructed uri', () => {
-//   expect.assertions(10)
-//   return getPreview('spotify:album:4tDBsfbHRJ9OdcMO9bmnai').then(result => {
-//     expect(result.date).toBe('2015-11-27')
-//     expect(result.description).toBeUndefined()
-//     expect(result.title).toBe('PRODUCT')
-//     expect(result.type).toBe('album')
-//     expect(result.artist).toContain('SOPHIE')
-//     expect(result.track).toBeDefined()
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toBeNull()
-//     expect(result.link).toContain('open.spotify.com/album/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
-//     )
-//   })
-// })
+  t.is(preview.date, undefined)
+  t.is(preview.description, undefined)
+  t.is(preview.title, 'SOPHIE – PRODUCT')
+  t.is(preview.type, 'playlist')
+  t.is(preview.artist.includes('SOPHIE'), true)
+  t.is(preview.track, 'BIPP')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio.includes('/mp3-preview/'), true)
+  t.is(preview.link.includes('/playlist/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:playlist:3Q4cPwMHY95ZHXtmcU2xvH'
+  )
+})
 
-// test('get preview for spotify album with play url', () => {
-//   expect.assertions(10)
-//   return getPreview(
-//     'https://play.spotify.com/album/4tDBsfbHRJ9OdcMO9bmnai'
-//   ).then(result => {
-//     expect(result.date).toBe('2015-11-27')
-//     expect(result.description).toBeUndefined()
-//     expect(result.title).toBe('PRODUCT')
-//     expect(result.type).toBe('album')
-//     expect(result.artist).toContain('SOPHIE')
-//     expect(result.track).toBeDefined()
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toBeNull()
-//     expect(result.link).toContain('open.spotify.com/album/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
-//     )
-//   })
-// })
+test('get preview for spotify episode', async t => {
+  const preview = await getPreview(
+    'http://open.spotify.com/episode/64TORH3xleuD1wcnFsrH1E'
+  )
 
-// xtest('get preview for spotify Album with twitter embed url', () => {
-//   expect.assertions(10)
-//   return getPreview(
-//     'https://open.spotify.com/embed/album/4tDBsfbHRJ9OdcMO9bmnai?utm_campaign=twitter-player&utm_source=open&utm_medium=twitter'
-//   ).then(result => {
-//     expect(result.date).toBe('2015-11-27')
-//     expect(result.description).toBeUndefined()
-//     expect(result.title).toBe('PRODUCT')
-//     expect(result.type).toBe('album')
-//     expect(result.artist).toContain('SOPHIE')
-//     expect(result.track).toBeDefined()
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toBeNull()
-//     expect(result.link).toContain('open.spotify.com/album/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
-//     )
-//   })
-// })
+  t.is(preview.title, 'Hasty Treat - Modules in Node')
+  t.is(preview.description.includes('In this Hasty Treat'), true)
+  t.is(preview.type, 'episode')
+  t.is(preview.artist.includes('Wes Bos & Scott Tolinski'), true)
+  t.is(preview.track, 'Syntax - Tasty Web Development Treats')
+  t.is(preview.date, '2020-01-06')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio.includes('/mp3-preview/'), true)
+  t.is(preview.link.includes('/episode/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:episode:64TORH3xleuD1wcnFsrH1E'
+  )
+})
 
-// test('get preview for spotify Track with constructed embed url', () => {
-//   expect.assertions(10)
-//   return getPreview(
-//     'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
-//   ).then(result => {
-//     expect(result.date).toBe('2015-11-27')
-//     expect(result.description).toBeUndefined()
-//     expect(result.title).toBe('PRODUCT')
-//     expect(result.type).toBe('album')
-//     expect(result.artist).toContain('SOPHIE')
-//     expect(result.track).toBeDefined()
-//     expect(result.image).toContain('://')
-//     expect(result.audio).toBeNull()
-//     expect(result.link).toContain('open.spotify.com/album/')
-//     expect(result.embed).toBe(
-//       'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
-//     )
-//   })
-// })
+test('get preview for spotify album with constructed uri', async t => {
+  const preview = await getPreview('spotify:album:4tDBsfbHRJ9OdcMO9bmnai')
+
+  t.is(preview.date, '2015-11-27')
+  t.is(preview.description, undefined)
+  t.is(preview.title, 'PRODUCT')
+  t.is(preview.type, 'album')
+  t.is(preview.artist.includes('SOPHIE'), true)
+  t.is(preview.track, 'BIPP')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio, null)
+  t.is(preview.link.includes('open.spotify.com/album/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
+  )
+})
+
+test('get preview for spotify album with play url', async t => {
+  const preview = await getPreview(
+    'https://play.spotify.com/album/4tDBsfbHRJ9OdcMO9bmnai'
+  )
+
+  t.is(preview.date, '2015-11-27')
+  t.is(preview.description, undefined)
+  t.is(preview.title, 'PRODUCT')
+  t.is(preview.type, 'album')
+  t.is(preview.artist.includes('SOPHIE'), true)
+  t.is(preview.track, 'BIPP')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio, null)
+  t.is(preview.link.includes('open.spotify.com/album/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
+  )
+})
+
+test('get preview for spotify Album with twitter embed url', async t => {
+  const preview = await getPreview(
+    'https://open.spotify.com/embed/album/4tDBsfbHRJ9OdcMO9bmnai?utm_campaign=twitter-player&utm_source=open&utm_medium=twitter'
+  )
+
+  t.is(preview.date, '2015-11-27')
+  t.is(preview.description, undefined)
+  t.is(preview.title, 'PRODUCT')
+  t.is(preview.type, 'album')
+  t.is(preview.artist.includes('SOPHIE'), true)
+  t.is(preview.track, 'BIPP')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio, null)
+  t.is(preview.link.includes('open.spotify.com/album/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
+  )
+})
+
+test('get preview for spotify Track with constructed embed url', async t => {
+  const preview = await getPreview(
+    'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
+  )
+
+  t.is(preview.date, '2015-11-27')
+  t.is(preview.description, undefined)
+  t.is(preview.title, 'PRODUCT')
+  t.is(preview.type, 'album')
+  t.is(preview.artist.includes('SOPHIE'), true)
+  t.is(preview.track, 'BIPP')
+  t.is(preview.image.includes('://'), true)
+  t.is(preview.audio, null)
+  t.is(preview.link.includes('open.spotify.com/album/'), true)
+  t.is(
+    preview.embed,
+    'https://embed.spotify.com/?uri=spotify:album:4tDBsfbHRJ9OdcMO9bmnai'
+  )
+})
